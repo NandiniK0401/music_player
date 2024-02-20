550819f96c670bc905364309ac8fcae5854e2aaa
w0501872 <w0501872@campus.nscc.ca>
2024-02-19 23:55:36 -0400

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+    {
+        name: "STAY",
+        artist: "Justin Bieber",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Mood",
+        artist: "24kGoldn",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Falling",
+        artist: "Trevor Daniel",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Stitches",
+        artist: "Shawn Mendes",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Happier",
+        artist: "Marshmello & Bastille",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Zara Sa",
+        artist: "KK",
+        img: "music-6",
+        src: "music-6"
+    },
+    
+    ];
